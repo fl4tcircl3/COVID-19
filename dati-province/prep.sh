f16d3f6fcaff41b769432876aa905e4aa732d77b
Davide Cordioli <d.cordioli@algebrasrl.com>
2020-03-12 10:55:53 +0100

    prepped

--- dati-province/prep.sh
+++ dati-province/prep.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 set -e
 
-sed '/In fase di definizione/d' ./dpc-covid19-ita-province-20200224.csv > dpc-covid19-ita-province-20200224-prepped.csv
-sed '/In fase di definizione/d' ./dpc-covid19-ita-province-20200225.csv > dpc-covid19-ita-province-20200225-prepped.csv
-sed '/In fase di definizione/d' ./dpc-covid19-ita-province-20200226.csv > dpc-covid19-ita-province-20200226-prepped.csv
-sed '/In fase di definizione/d' ./dpc-covid19-ita-province-20200227.csv > dpc-covid19-ita-province-20200227-prepped.csv
-sed '/In fase di definizione/d' ./dpc-covid19-ita-province-20200228.csv > dpc-covid19-ita-province-20200228-prepped.csv
-sed '/In fase di definizione/d' ./dpc-covid19-ita-province-20200229.csv > dpc-covid19-ita-province-20200229-prepped.csv
-sed '/In fase di definizione/d' ./dpc-covid19-ita-province-20200301.csv > dpc-covid19-ita-province-20200301-prepped.csv
-sed '/In fase di definizione/d' ./dpc-covid19-ita-province-20200302.csv > dpc-covid19-ita-province-20200302-prepped.csv
-sed '/In fase di definizione/d' ./dpc-covid19-ita-province-20200303.csv > dpc-covid19-ita-province-20200303-prepped.csv
-sed '/In fase di definizione/d' ./dpc-covid19-ita-province-20200304.csv > dpc-covid19-ita-province-20200304-prepped.csv
-sed '/In fase di definizione/d' ./dpc-covid19-ita-province-20200305.csv > dpc-covid19-ita-province-20200305-prepped.csv
-sed '/In fase di definizione/d' ./dpc-covid19-ita-province-20200306.csv > dpc-covid19-ita-province-20200306-prepped.csv
-sed '/In fase di definizione/d' ./dpc-covid19-ita-province-20200307.csv > dpc-covid19-ita-province-20200307-prepped.csv
-sed '/In fase di definizione/d' ./dpc-covid19-ita-province-20200308.csv > dpc-covid19-ita-province-20200308-prepped.csv
-sed '/In fase di definizione/d' ./dpc-covid19-ita-province-20200309.csv > dpc-covid19-ita-province-20200309-prepped.csv
-sed '/In fase di definizione/d' ./dpc-covid19-ita-province-20200310.csv > dpc-covid19-ita-province-20200310-prepped.csv
-sed '/In fase di definizione/d' ./dpc-covid19-ita-province-20200311.csv > dpc-covid19-ita-province-20200311-prepped.csv
+sed '/In fase di definizione/d' ./dpc-covid19-ita-province-20200224.csv > ./prepped/dpc-covid19-ita-province-20200224-prepped.csv
+sed '/In fase di definizione/d' ./dpc-covid19-ita-province-20200225.csv > ./prepped/dpc-covid19-ita-province-20200225-prepped.csv
+sed '/In fase di definizione/d' ./dpc-covid19-ita-province-20200226.csv > ./prepped/dpc-covid19-ita-province-20200226-prepped.csv
+sed '/In fase di definizione/d' ./dpc-covid19-ita-province-20200227.csv > ./prepped/dpc-covid19-ita-province-20200227-prepped.csv
+sed '/In fase di definizione/d' ./dpc-covid19-ita-province-20200228.csv > ./prepped/dpc-covid19-ita-province-20200228-prepped.csv
+sed '/In fase di definizione/d' ./dpc-covid19-ita-province-20200229.csv > ./prepped/dpc-covid19-ita-province-20200229-prepped.csv
+sed '/In fase di definizione/d' ./dpc-covid19-ita-province-20200301.csv > ./prepped/dpc-covid19-ita-province-20200301-prepped.csv
+sed '/In fase di definizione/d' ./dpc-covid19-ita-province-20200302.csv > ./prepped/dpc-covid19-ita-province-20200302-prepped.csv
+sed '/In fase di definizione/d' ./dpc-covid19-ita-province-20200303.csv > ./prepped/dpc-covid19-ita-province-20200303-prepped.csv
+sed '/In fase di definizione/d' ./dpc-covid19-ita-province-20200304.csv > ./prepped/dpc-covid19-ita-province-20200304-prepped.csv
+sed '/In fase di definizione/d' ./dpc-covid19-ita-province-20200305.csv > ./prepped/dpc-covid19-ita-province-20200305-prepped.csv
+sed '/In fase di definizione/d' ./dpc-covid19-ita-province-20200306.csv > ./prepped/dpc-covid19-ita-province-20200306-prepped.csv
+sed '/In fase di definizione/d' ./dpc-covid19-ita-province-20200307.csv > ./prepped/dpc-covid19-ita-province-20200307-prepped.csv
+sed '/In fase di definizione/d' ./dpc-covid19-ita-province-20200308.csv > ./prepped/dpc-covid19-ita-province-20200308-prepped.csv
+sed '/In fase di definizione/d' ./dpc-covid19-ita-province-20200309.csv > ./prepped/dpc-covid19-ita-province-20200309-prepped.csv
+sed '/In fase di definizione/d' ./dpc-covid19-ita-province-20200310.csv > ./prepped/dpc-covid19-ita-province-20200310-prepped.csv
+sed '/In fase di definizione/d' ./dpc-covid19-ita-province-20200311.csv > ./prepped/dpc-covid19-ita-province-20200311-prepped.csv
