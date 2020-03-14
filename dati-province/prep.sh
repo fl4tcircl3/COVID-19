e60fb07b7bd5f6d0829a38d5ba6fa4d50556df2e
Davide Cordioli <davide.cordioli@gmail.com>
2020-03-14 16:58:56 +0100

    prepping

--- dati-province/prep.sh
+++ dati-province/prep.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
 
-find . -maxdepth 1 -name dpc-covid19-ita-province-\*.csv -exec sh -c 'sed -e "/In fase di definizione/d" -e "s/ï¿½/ì/g" {} > ./prepped/{}' \;
+find . -maxdepth 1 -name dpc-covid19-ita-province-\*.csv \
+  -exec sh -c 'sed -e "/In fase di definizione/d" -e "s/ï¿½/ì/g" {} > ./prepped/{}' \;
